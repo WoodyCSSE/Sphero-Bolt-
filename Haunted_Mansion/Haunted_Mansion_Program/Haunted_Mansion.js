f5e0ea5914865d3068a03dc54c2ab00af9bd0fc1
Woody Tilbury <02sherwood.tilbury@gulllakecs.org>
2022-01-26 09:36:23 -0800

    Initial Haunted Mansion Commit

---
+++ Haunted_Mansion/Haunted_Mansion_Program/Haunted_Mansion.js
@@ -0,0 +1,6 @@
+/*
+Programer: Woody Tilbury
+Team Member: Landon
+Date: 1.26.2022
+Program: We are making program to go through a haunted mansion maze filled with hauned sounds, changing colors, and other things using a java script.
+*/
